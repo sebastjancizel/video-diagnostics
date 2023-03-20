792bb638864fe9e2b722fc4ecb7dfb96f5467db4
Sebastjan <sebastjancizel@gmail.com>
2023-03-20 21:31:04 +0000

    Add navbar; cleanup

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,80 +1,80 @@
 <template>
 	<div class="video-compare-container" ref="container">
-	  <video class="video-main" loop autoplay muted controls ref="mainVideo">
-		<source :src="mainVideoSrc" type="video/mp4">
-	  </video>
-	  <div class="video-clipper" ref="clipper">
-		<video class="video-clipped" loop autoplay muted controls ref="clippedVideo">
-		  <source :src="clippedVideoSrc" type="video/mp4">
+		<video class="video-main" loop autoplay muted controls ref="mainVideo">
+			<source :src="mainVideoSrc" type="video/mp4">
 		</video>
-	  </div>
+		<div class="video-clipper" ref="clipper">
+			<video class="video-clipped" loop autoplay muted controls ref="clippedVideo">
+				<source :src="clippedVideoSrc" type="video/mp4">
+			</video>
+		</div>
 	</div>
-  </template>
+</template>
 
-  <script>
-  export default {
+<script>
+export default {
 	data() {
-	  return {
-		mainVideoSrc: '/src/assets/test2.mp4',
-		clippedVideoSrc: '/src/assets/test.mp4'
-	  }
+		return {
+			mainVideoSrc: '/src/assets/test2.mp4',
+			clippedVideoSrc: '/src/assets/test.mp4'
+		}
 	},
 	mounted() {
-	  this.trackLocation = this.trackLocation.bind(this);
-	  this.videoContainer = this.$refs.container;
-	  this.videoClipper = this.$refs.clipper;
-	  this.clippedVideo = this.$refs.clippedVideo;
-	  this.videoContainer.addEventListener('mousemove', this.trackLocation, false);
-	  this.videoContainer.addEventListener('touchstart', this.trackLocation, false);
-	  this.videoContainer.addEventListener('touchmove', this.trackLocation, false);
+		this.trackLocation = this.trackLocation.bind(this);
+		this.videoContainer = this.$refs.container;
+		this.videoClipper = this.$refs.clipper;
+		this.clippedVideo = this.$refs.clippedVideo;
+		this.videoContainer.addEventListener('mousemove', this.trackLocation, false);
+		this.videoContainer.addEventListener('touchstart', this.trackLocation, false);
+		this.videoContainer.addEventListener('touchmove', this.trackLocation, false);
 	},
 	methods: {
-	  trackLocation(e) {
-		const rect = this.videoContainer.getBoundingClientRect();
-		const position = ((e.pageX - rect.left) / this.videoContainer.offsetWidth) * 100;
-		if (position <= 100) {
-		  this.videoClipper.style.width = position + '%';
-		  this.clippedVideo.style.width = (100 / position) * 100 + '%';
+		trackLocation(e) {
+			const rect = this.videoContainer.getBoundingClientRect();
+			const position = ((e.pageX - rect.left) / this.videoContainer.offsetWidth) * 100;
+			if (position <= 100) {
+				this.videoClipper.style.width = position + '%';
+				this.clippedVideo.style.width = (100 / position) * 100 + '%';
+			}
 		}
-	  }
 	}
-  }
-  </script>
+}
+</script>
 
-  <style scoped>
-  .video-compare-container {
+<style scoped>
+.video-compare-container {
 	background: #333;
-	margin: 2rem;
+	margin: 0 auto;
 	display: inline-block;
 	line-height: 0;
 	position: relative;
 	width: 90vw;
 	padding-top: 42.3%;
-  }
+}
 
-  .video-main {
+.video-main {
 	width: 100%;
 	position: absolute;
 	top: 0;
 	height: 100%;
-  }
+}
 
-  .video-main::-webkit-media-controls {
+.video-main::-webkit-media-controls {
 	position: relative;
 	z-index: 3;
-  }
+}
 
-  .video-clipper {
+.video-clipper {
 	width: 50%;
 	position: absolute;
 	top: 0;
 	bottom: 0;
 	overflow: hidden;
-  }
+}
 
-  .video-clipped {
+.video-clipped {
 	width: 200%;
 	height: 100%;
 	z-index: 1;
-  }
-  </style>
+}
+</style>
