27d7a5d050867434f596f7401a73949c28ede24d
Sebastjan <sebastjancizel@gmail.com>
2023-04-06 21:28:42 +0100

    Clean-up

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import VueVideoPlayer from '@videojs-player/vue'
 
 import './assets/main.css'
 
 const app = createApp(App)
-
-// app.use(router)
-app.use(VueVideoPlayer)
-
 app.mount('#app')
